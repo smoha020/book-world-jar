193b6ef48307a696279dd2dfe4344e6f59563398
Said Mohamed <said.mohamed@fewidev.com>
2022-09-20 16:20:38 -0500

    manifests path

--- k8-deploy.sh
+++ k8-deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #CREATE SECRETS
-kubectl apply -f mysql-secrets.yaml
+kubectl apply -f /mysql-secrets.yaml
 
 #APPLY MYSQL DEPLOYMENTS AND SERVICES
-kubectl apply -f mysql-manifests.yaml
+kubectl apply -f /mysql-manifests.yaml
 
 #APPLY SPRING BOOT APP DEPLOYMENTS AND SERVICES
-kubectl apply -f app-manifests.yaml
+kubectl apply -f /app-manifests.yaml
 
 #LAUNCH INGRESS CONTROLLER
-kubectl apply -f ingress-manifests.yaml
+kubectl apply -f /ingress-manifests.yaml
 
 #APPLY INGRESS RULES
-kubectl apply ingress-rules.yaml
+kubectl apply /ingress-rules.yaml
