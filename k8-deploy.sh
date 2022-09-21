da98e7a018e695450bf8e8a6d2b6cb5d5e6e23dc
Said Mohamed <said.mohamed@fewidev.com>
2022-09-21 13:50:08 -0500

    sleep command

--- k8-deploy.sh
+++ k8-deploy.sh
@@ -16,5 +16,8 @@ kubectl apply -f k8s/app-manifests.yaml
 #LAUNCH INGRESS CONTROLLER
 kubectl apply -f k8s/ingress-manifests.yaml
 
+#WAIT FOR INGRESS CONTROLLER TO BE CREATED
+sleep 1m
+
 #APPLY INGRESS RULES
 kubectl apply -f k8s/ingress-rules.yaml
