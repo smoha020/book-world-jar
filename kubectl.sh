a0e27ed40b7f763e23661bc3f3e2153666ced1a1
root <root@ip-172-31-29-65.us-east-2.compute.internal>
2023-07-14 16:19:17 +0000

    aws cli

--- kubectl.sh
+++ kubectl.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-#THIS SCRIPT WILL INSTALL KUBECTL IF YOU DON'T HAVE IT ALREADY
+#THIS SCRIPT WILL INSTALL KUBECTL IF YOU DON'T HAVE IT ALREADY (FROM KUBERNETES WEBSITE)
 
-curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
+#1. Download latest release with the command:
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+
+
+#2. Validate the binary (optional)
+
+#Download the kubectl checksum file:
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+
+#Validate the kubectl binary against the checksum file:
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+
+#3. Install kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#c. Apply execute permissions to the binary.
 
 chmod +x ./kubectl
 
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 
-kubectl version --short --client
+#d. If you do not have root access on the target system, you can still install kubectl to the ~/.local/bin directory:
+
+#chmod +x kubectl
+#mkdir -p ~/.local/bin
+#mv ./kubectl ~/.local/bin/kubectl
+# and then append (or prepend) ~/.local/bin to $PATH
+
+#Test to ensure the version you installed is up-to-date:
+kubectl version --client
