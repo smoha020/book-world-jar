33c9e488de080b259d73941a67e3cac9110456f9
Ubuntu <ubuntu@ip-172-31-7-53.us-east-2.compute.internal>
2023-07-25 19:35:24 +0000

    IODC and IAM

--- iam-efs-csi.sh
+++ iam-efs-csi.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+
+###############CHECK FOR IODC########################
+
+#1. Determine whether you have an existing IAM OIDC provider for your cluster.
+#Retrieve your cluster's OIDC provider ID and store it in a variable. Replace my-cluster with your own value.
+
+export cluster_name=springbootMysqlCluster
+oidc_id=$(aws eks describe-cluster --name $cluster_name --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
+
+#2. Determine whether an IAM OIDC provider with your cluster's ID is already in your account.
+
+aws iam list-open-id-connect-providers | grep $oidc_id | cut -d "/" -f4
+
+
+#If output is returned, then you already have an IAM OIDC provider for your cluster and you can 
+#skip the next step. If no output is returned, then you must create an IAM OIDC provider for your cluster.
+
+#3.Create an IAM OIDC identity provider for your cluster with the following command.
+
+eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve
+
+
+
+
+####################IAM POLICY######################
+
 #1. Create an IAM policy that allows the CSI driver's service account to make calls to AWS APIs on your behalf.
 
 #i. Download the IAM policy document.
@@ -22,7 +48,7 @@ aws iam create-policy \
 #cluster is in. If your cluster is in the AWS GovCloud (US-East) or AWS GovCloud (US-West) AWS Regions, then replace arn:aws: with arn:aws-us-gov:.
 
 eksctl create iamserviceaccount \
-    --cluster my-cluster \
+    --cluster springbootMysqlCluster \
     --namespace kube-system \
     --name efs-csi-controller-sa \
     --attach-policy-arn arn:aws:iam::348815537453:policy/EKS_EFS_CSI_Driver_Policy \
