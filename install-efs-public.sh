dc64bea52372aa754f9b9905449e3c9a111c0113
Ubuntu <ubuntu@ip-172-31-7-53.us-east-2.compute.internal>
2023-07-25 20:29:20 +0000

    installed public efs

---
+++ install-efs-public.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#1. Install from public registry
+
+kubectl kustomize \
+    "github.com/kubernetes-sigs/aws-efs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.5" > public-ecr-driver.yaml
+
+
+
+#2. If you already created a service account by following Create an IAM policy and role, then edit the 
+#public-ecr-driver.yaml file. Remove the following lines that create a Kubernetes service account.
+
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  labels:
+    app.kubernetes.io/name: aws-efs-csi-driver
+  name: efs-csi-controller-sa
+  namespace: kube-system
+---
+
+
+#3. apply the file
+
+#kubectl apply -f public-ecr-driver.yaml
